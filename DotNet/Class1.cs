a5450321dea2916f33203801f24e63b9a5c3d9c5
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-29 08:32:35 -0600

    C# class library

---
+++ DotNet/Class1.cs
@@ -0,0 +1,13 @@
+﻿﻿using System.Runtime.InteropServices;
+
+namespace hellonativeaot;
+
+public class Class1
+{
+    [UnmanagedCallersOnly(EntryPoint = "ManagedAdd")]
+    public static int ManagedAdd(int x, int y)
+    {
+        Console.WriteLine($"ManagedAdd({x}, {y})");
+        return x + y;
+    }
+}
