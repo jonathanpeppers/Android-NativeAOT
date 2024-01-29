8cee5394b15ae5895c0bcd2ba929c1a3efcf652b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-29 17:11:10 -0600

    WIP

---
+++ DotNet/Renderer.cs
@@ -0,0 +1,20 @@
+﻿﻿using Android;
+using System.Runtime.InteropServices;
+using static Android.NativeMethods;
+
+namespace hellonativeaot;
+
+public class Renderer
+{
+    [UnmanagedCallersOnly(EntryPoint = "Render")]
+    public static void Render()
+    {
+        // NOTE: doesn't work as stdio doesn't appear in Android logcat output
+        // Console.WriteLine($"ManagedAdd({x}, {y})");
+        
+        // p/invoke to call __android_log_print
+        LogPrint(LogPriority.Info, "Managed", "Render()");
+
+        return x + y;
+    }
+}
--- Native/app/src/main/cpp/native-lib.cpp
+++ Native/app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,137 @@
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+#include <cassert>
+#include <memory>
 #include <android_native_app_glue.h>
 #include <jni.h>
 #include <android/log.h>
 #include "dotnet.h"
 
+/**
+ * Shared state for our app.
+ */
+struct engine {
+    struct android_app* app;
+
+    int animating;
+    EGLDisplay display;
+    EGLSurface surface;
+    EGLContext context;
+    int32_t width;
+    int32_t height;
+};
+
+/**
+* Initialize an EGL context for the current display.
+*/
+static int engine_init_display(struct engine* engine) {
+    // initialize OpenGL ES and EGL
+
+    /*
+     * Here specify the attributes of the desired configuration.
+     * Below, we select an EGLConfig with at least 8 bits per color
+     * component compatible with on-screen windows
+     */
+    const EGLint attribs[] = {EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+                              EGL_BLUE_SIZE,    8,
+                              EGL_GREEN_SIZE,   8,
+                              EGL_RED_SIZE,     8,
+                              EGL_NONE};
+    EGLint w, h, format;
+    EGLint numConfigs;
+    EGLConfig config = nullptr;
+    EGLSurface surface;
+    EGLContext context;
+
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+    eglInitialize(display, nullptr, nullptr);
+
+    /* Here, the application chooses the configuration it desires.
+     * find the best match if possible, otherwise use the very first one
+     */
+    eglChooseConfig(display, attribs, nullptr, 0, &numConfigs);
+    std::unique_ptr<EGLConfig[]> supportedConfigs(new EGLConfig[numConfigs]);
+    assert(supportedConfigs);
+    eglChooseConfig(display, attribs, supportedConfigs.get(), numConfigs,
+                    &numConfigs);
+    assert(numConfigs);
+    auto i = 0;
+    for (; i < numConfigs; i++) {
+        auto& cfg = supportedConfigs[i];
+        EGLint r, g, b, d;
+        if (eglGetConfigAttrib(display, cfg, EGL_RED_SIZE, &r) &&
+            eglGetConfigAttrib(display, cfg, EGL_GREEN_SIZE, &g) &&
+            eglGetConfigAttrib(display, cfg, EGL_BLUE_SIZE, &b) &&
+            eglGetConfigAttrib(display, cfg, EGL_DEPTH_SIZE, &d) && r == 8 &&
+            g == 8 && b == 8 && d == 0) {
+            config = supportedConfigs[i];
+            break;
+        }
+    }
+    if (i == numConfigs) {
+        config = supportedConfigs[0];
+    }
+
+    if (config == nullptr) {
+        __android_log_print(ANDROID_LOG_WARN, "Native", "Unable to initialize EGLConfig");
+        return -1;
+    }
+
+    /* EGL_NATIVE_VISUAL_ID is an attribute of the EGLConfig that is
+     * guaranteed to be accepted by ANativeWindow_setBuffersGeometry().
+     * As soon as we picked a EGLConfig, we can safely reconfigure the
+     * ANativeWindow buffers to match, using EGL_NATIVE_VISUAL_ID. */
+    eglGetConfigAttrib(display, config, EGL_NATIVE_VISUAL_ID, &format);
+    surface =
+            eglCreateWindowSurface(display, config, engine->app->window, nullptr);
+    context = eglCreateContext(display, config, nullptr, nullptr);
+
+    if (eglMakeCurrent(display, surface, surface, context) == EGL_FALSE) {
+        __android_log_print(ANDROID_LOG_WARN, "Native", "Unable to eglMakeCurrent");
+        return -1;
+    }
+
+    eglQuerySurface(display, surface, EGL_WIDTH, &w);
+    eglQuerySurface(display, surface, EGL_HEIGHT, &h);
+
+    engine->display = display;
+    engine->context = context;
+    engine->surface = surface;
+    engine->width = w;
+    engine->height = h;
+
+    // Check openGL on the system
+    auto opengl_info = {GL_VENDOR, GL_RENDERER, GL_VERSION, GL_EXTENSIONS};
+    for (auto name : opengl_info) {
+        auto info = glGetString(name);
+        __android_log_print(ANDROID_LOG_INFO, "Native", "OpenGL Info: %s", info);
+    }
+    // Initialize GL state.
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
+    glEnable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_DEPTH_TEST);
+
+    return 0;
+}
+
 extern "C" {
 
     void handle_cmd(android_app *pApp, int32_t cmd) {
+        auto* engine = (struct engine*)pApp->userData;
+        switch (cmd) {
+            case APP_CMD_INIT_WINDOW:
+                // The window is being shown, get it ready.
+                if (engine->app->window != nullptr) {
+                    engine_init_display(engine);
+                    //TODO:
+                    //engine_draw_frame(engine);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     void android_main(struct android_app *pApp) {
