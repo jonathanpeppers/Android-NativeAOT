2255cadfeb18f0aab50b274c04fe2082fdec5be7
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-29 16:05:59 -0600

    p/invoke to write to `adb logcat` (#1)
    `Console.WriteLine()` won't work as-is, because it writes to Unix stdout.
    
    Unix stdout doesn't appear in `adb logcat`:
    
    * https://codelab.wordpress.com/2014/11/03/how-to-use-standard-output-streams-for-logging-in-android-apps/
    
    So we can instead p/invoke into:
    
    ```csharp
    [DllImport("log", EntryPoint = "__android_log_print", CallingConvention = CallingConvention.Cdecl)]
    public static extern int LogPrint(LogPriority priority, string tag, string format);
    ```

---
+++ DotNet/Android.cs
@@ -0,0 +1,54 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace Android;
+
+/// <summary>
+/// Android log priority values, in increasing order of priority.
+/// from Android's log.h
+/// </summary>
+enum LogPriority
+{
+    /// <summary>
+    /// For internal use only.
+    /// </summary>
+    Unknown = 0,
+    /// <summary>
+    /// The default priority, for internal use only.
+    /// </summary>
+    Default, /* only for SetMinPriority() */
+    /// <summary>
+    /// Verbose logging. Should typically be disabled for a release apk.
+    /// </summary>
+    Verbose,
+    /// <summary>
+    /// Debug logging. Should typically be disabled for a release apk.
+    /// </summary>
+    Debug,
+    /// <summary>
+    /// Informational logging. Should typically be disabled for a release apk.
+    /// </summary>
+    Info,
+    /// <summary>
+    /// Warning logging. For use with recoverable failures.
+    /// </summary>
+    Warn,
+    /// <summary>
+    /// Error logging. For use with unrecoverable failures.
+    /// </summary>
+    Error,
+    /// <summary>
+    /// Fatal logging. For use when aborting.
+    /// </summary>
+    Fatal,
+    /// <summary>
+    /// For internal use only.
+    /// </summary>
+    Silent, /* only for SetMinPriority(); must be last */
+}
+
+static class NativeMethods
+{
+    [DllImport("log", EntryPoint = "__android_log_print", CallingConvention = CallingConvention.Cdecl)]
+    public static extern int LogPrint(LogPriority priority, string tag, string format);
+}
--- DotNet/Class1.cs
+++ DotNet/Class1.cs
@@ -1,4 +1,6 @@
-﻿﻿using System.Runtime.InteropServices;
+﻿﻿using Android;
+using System.Runtime.InteropServices;
+using static Android.NativeMethods;
 
 namespace hellonativeaot;
 
@@ -7,7 +9,12 @@ public class Class1
     [UnmanagedCallersOnly(EntryPoint = "ManagedAdd")]
     public static int ManagedAdd(int x, int y)
     {
-        Console.WriteLine($"ManagedAdd({x}, {y})");
+        // NOTE: doesn't work as stdio doesn't appear in Android logcat output
+        // Console.WriteLine($"ManagedAdd({x}, {y})");
+        
+        // p/invoke to call __android_log_print
+        LogPrint(LogPriority.Info, "Managed", $"ManagedAdd({x}, {y})");
+
         return x + y;
     }
 }
