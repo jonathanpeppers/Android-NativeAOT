53a022019a3ec4fbf26ae28c2d78854d0f16e750
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-29 21:55:30 +0000

    Log to file

--- DotNet/Class1.cs
+++ DotNet/Class1.cs
@@ -11,8 +11,15 @@ public class Class1
     {
         // NOTE: doesn't work as stdio doesn't appear in Android logcat output
         // Console.WriteLine($"ManagedAdd({x}, {y})");
-
-        LogPrint(LogPriority.Info, "Managed", $"ManagedAdd({x}, {y})");
+        try
+        {
+            LogPrint(LogPriority.Info, "Managed", $"ManagedAdd({x}, {y})");
+        }
+        catch (Exception exc)
+        {
+            File.AppendAllText ("/data/user/0/com.jonathanpeppers.nativeaot/files/log.txt", exc.ToString() + "\n");
+        }
+        File.AppendAllText ("/data/user/0/com.jonathanpeppers.nativeaot/files/log.txt", $"ManagedAdd({x}, {y})\n");
 
         return x + y;
     }
