5f0a86aefe2499f495b121f4f331b0b3c042d937
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-30 15:38:56 +0000

    WIP managed side

--- DotNet/Renderer.cs
+++ DotNet/Renderer.cs
@@ -25,66 +25,78 @@ public class Renderer
     public static void Render()
     {
         LogPrint(LogPriority.Info, "Managed", "Render()");
-
-        GLES20.glClear(GLES20.COLOR_BUFFER_BIT | GLES20.DEPTH_BUFFER_BIT | GLES20.STENCIL_BUFFER_BIT);
-
-        // create the contexts if not done already
-        if (context == null)
+        try
         {
-            var glInterface = GRGlInterface.Create();
-            context = GRContext.CreateGl(glInterface);
-        }
+            GLES20.glClear(GLES20.COLOR_BUFFER_BIT | GLES20.DEPTH_BUFFER_BIT | GLES20.STENCIL_BUFFER_BIT);
 
-        // manage the drawing surface
-        if (renderTarget == null || lastSize != newSize || !renderTarget.IsValid)
+            // create the contexts if not done already
+            if (context == null)
+            {
+                var glInterface = GRGlInterface.Create();
+                context = GRContext.CreateGl(glInterface);
+            }
+
+            // manage the drawing surface
+            if (renderTarget == null || lastSize != newSize || !renderTarget.IsValid)
+            {
+                // create or update the dimensions
+                lastSize = newSize;
+
+                // read the info from the buffer
+                GLES20.glGetIntegerv(GLES20.FRAMEBUFFER_BINDING, out int frame);
+                GLES20.glGetIntegerv(GLES20.STENCIL_BITS, out int stencil);
+                GLES20.glGetIntegerv(GLES20.SAMPLES, out int samples);
+                var maxSamples = context.GetMaxSurfaceSampleCount(colorType);
+                if (samples > maxSamples)
+                    samples = maxSamples;
+                glInfo = new GRGlFramebufferInfo((uint)frame, colorType.ToGlSizedFormat());
+
+                // destroy the old surface
+                surface?.Dispose();
+                surface = null;
+                canvas = null;
+
+                // re-create the render target
+                renderTarget?.Dispose();
+                renderTarget = new GRBackendRenderTarget(newSize.Width, newSize.Height, samples, stencil, glInfo);
+            }
+
+            // create the surface
+            if (surface == null)
+            {
+                surface = SKSurface.Create(context, renderTarget, surfaceOrigin, colorType);
+                canvas = surface.Canvas;
+            }
+
+            using (new SKAutoCanvasRestore(canvas, true))
+            {
+                // start drawing
+            }
+
+            // flush the SkiaSharp contents to GL
+            canvas?.Flush();
+            context.Flush();
+        }
+        catch (Exception exc)
         {
-            // create or update the dimensions
-            lastSize = newSize;
-
-            // read the info from the buffer
-            GLES20.glGetIntegerv(GLES20.FRAMEBUFFER_BINDING, out int frame);
-            GLES20.glGetIntegerv(GLES20.STENCIL_BITS, out int stencil);
-            GLES20.glGetIntegerv(GLES20.SAMPLES, out int samples);
-            var maxSamples = context.GetMaxSurfaceSampleCount(colorType);
-            if (samples > maxSamples)
-                samples = maxSamples;
-            glInfo = new GRGlFramebufferInfo((uint)frame, colorType.ToGlSizedFormat());
-
-            // destroy the old surface
-            surface?.Dispose();
-            surface = null;
-            canvas = null;
-
-            // re-create the render target
-            renderTarget?.Dispose();
-            renderTarget = new GRBackendRenderTarget(newSize.Width, newSize.Height, samples, stencil, glInfo);
+            LogPrint(LogPriority.Error, "Managed", $"Exception in Render(): {exc}");
         }
-
-        // create the surface
-        if (surface == null)
-        {
-            surface = SKSurface.Create(context, renderTarget, surfaceOrigin, colorType);
-            canvas = surface.Canvas;
-        }
-
-        using (new SKAutoCanvasRestore(canvas, true))
-        {
-            // start drawing
-        }
-
-        // flush the SkiaSharp contents to GL
-        canvas?.Flush();
-        context.Flush();
     }
 
     [UnmanagedCallersOnly(EntryPoint = "Resize")]
     public static void Resize(int width, int height)
     {
         LogPrint(LogPriority.Info, "Managed", $"Resize({width}, {height})");
+        try
+        {
+            GLES20.glViewport(0, 0, width, height);
 
-        GLES20.glViewport(0, 0, width, height);
-
-        // get the new surface size
-        newSize = new SKSizeI(width, height);
+            // get the new surface size
+            newSize = new SKSizeI(width, height);
+        }
+        catch (Exception exc)
+        {
+            LogPrint(LogPriority.Error, "Managed", $"Exception in Resize(): {exc}");
+        }
     }
 }
