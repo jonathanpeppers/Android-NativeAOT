2e1b77de3dc3520d54565a1e093755728a9dc2aa
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-30 14:32:37 -0600

    Fix sizes

--- DotNet/Renderer.cs
+++ DotNet/Renderer.cs
@@ -30,7 +30,7 @@ public class Renderer
     };
     static readonly SKFont fpsFont = new()
     {
-        Size = 10f,
+        Size = 44f,
     };
     static SKRuntimeShaderBuilder? currentEffectBuilder;
     static SKShader? currentShader;
@@ -132,7 +132,7 @@ public class Renderer
                 currentPaint.Shader = currentShader;
 
                 canvas.DrawRect(0, 0, lastSize.Width, lastSize.Height, currentPaint);
-                canvas.DrawText($"{frameCounter.Rate:0.0} fps", 10, 100, fpsFont, fpsPaint);
+                canvas.DrawText($"{frameCounter.Rate:0.0} fps", 40, 200, fpsFont, fpsPaint);
             }
 
             // flush the SkiaSharp contents to GL
