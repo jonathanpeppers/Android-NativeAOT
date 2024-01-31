8fbcf656783e0f0a96bf61066e0d1d0d3f99793f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-31 10:02:29 -0600

    Setup GitHub Actions (#3)
    Context: https://github.com/devcontainers/ci/blob/main/docs/github-action.md

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,4 +2,4 @@
 
 echo "ANDROID_HOME=$ANDROID_HOME"
 
-yes | sudo $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "ndk;23.2.8568313"
+yes | sudo $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "ndk;23.2.8568313" "cmake;3.22.1"
