f987d1dbe2daee940506ed4e5243b37cefec37de
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-01-31 14:45:46 +0000

    Different shaders

--- DotNet/Shaders.cs
+++ DotNet/Shaders.cs
@@ -44,165 +44,50 @@ static class Shaders
                 }
                 """,
             
-            // https://shaders.skia.org/?id=ed72577c437c036447372e4c873462fc1bbfc0cb5e9fb0630ab1c07368a0db48
-            // Source: @zozuar https://twitter.com/zozuar/status/1482754721450446850
+            // https://shaders.skia.org/?id=2bee4488820c3253cd8861e85ce3cab86f482cfddd79cfd240591bf64f7bcc38
+            // Source: @XorDev https://twitter.com/XorDev/status/1475524322785640455
             1 => """
-                mat2 rotate2D(float r){
-                    return mat2(cos(r), sin(r), -sin(r), cos(r));
-                }
-
-                mat3 rotate3D(float angle, vec3 axis){
-                    vec3 a = normalize(axis);
-                    float s = sin(angle);
-                    float c = cos(angle);
-                    float r = 1.0 - c;
-                    return mat3(
-                        a.x * a.x * r + c,
-                        a.y * a.x * r + a.z * s,
-                        a.z * a.x * r - a.y * s,
-                        a.x * a.y * r - a.z * s,
-                        a.y * a.y * r + c,
-                        a.z * a.y * r + a.x * s,
-                        a.x * a.z * r + a.y * s,
-                        a.y * a.z * r - a.x * s,
-                        a.z * a.z * r + c
-                    );
-                }
-
-                half4 main(float2 FC) {
+                vec4 main(vec2 FC) {
                     vec4 o = vec4(0);
-                    vec2 r = iResolution.xy;
-                    vec3 v = vec3(1,3,7), p = vec3(0);
-                    float t=iTime, n=0, e=0, g=0, k=t*.2;
-                    for (float i=0; i<100; ++i) {
-                        p = vec3((FC.xy-r*.5)/r.y*g,g)*rotate3D(k,cos(k+v));
-                        p.z += t;
-                        p = asin(sin(p)) - 3.;
-                        n = 0;
-                        for (float j=0; j<9.; ++j) {
-                        p.xz *= rotate2D(g/8.);
-                        p = abs(p);
-                        p = p.x<p.y ? n++, p.zxy : p.zyx;
-                        p += p-v;
-                        }
-                        g += e = max(p.x,p.z) / 1e3 - .01;
-                        o.rgb += .1/exp(cos(v*g*.1+n)+3.+1e4*e);
+                    vec2 p = vec2(0), c=p, u=FC.xy*2.-iResolution.xy;
+                    float a;
+                    for (float i=0; i<4e2; i++) {
+                        a = i/2e2-1.;
+                        p = cos(i*2.4+iTime+vec2(0,11))*sqrt(1.-a*a);
+                        c = u/iResolution.y+vec2(p.x,a)/(p.y+2.);
+                        o += (cos(i+vec4(0,2,4,0))+1.)/dot(c,c)*(1.-p.y)/3e4;
                     }
-                    return o.xyz1;
+                    return o;
                 }
                 """,
 
-            // https://shaders.skia.org/?id=f9be5248170044ea1b69ee78456eec4be98d3e71a6c61fd0138f6018abda2ac3
+            // https://shaders.skia.org/?id=23a360c975c3cb195c89ccdf65ec549e279ce8a959643b447e69cb70614a6eca
+            // Source: @zozuar https://twitter.com/zozuar/status/1492217553103503363
             2 => """
-                const float cloudscale = 1.1;
-                const float speed = 0.03;
-                const float clouddark = 0.5;
-                const float cloudlight = 0.3;
-                const float cloudcover = 0.2;
-                const float cloudalpha = 8.0;
-                const float skytint = 0.5;
-                const vec3 skycolour1 = vec3(0.2, 0.4, 0.6);
-                const vec3 skycolour2 = vec3(0.4, 0.7, 1.0);
-
-                const mat2 m = mat2( 1.6,  1.2, -1.2,  1.6 );
-
-                vec2 hash( vec2 p ) {
-                    p = vec2(dot(p,vec2(127.1,311.7)), dot(p,vec2(269.5,183.3)));
-                    return -1.0 + 2.0*fract(sin(p)*43758.5453123);
+                vec3 hsv(float h, float s, float v){
+                    vec4 t = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+                    vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
+                    return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
                 }
 
-                float noise( in vec2 p ) {
-                    const float K1 = 0.366025404; // (sqrt(3)-1)/2;
-                    const float K2 = 0.211324865; // (3-sqrt(3))/6;
-                    vec2 i = floor(p + (p.x+p.y)*K1);	
-                    vec2 a = p - i + (i.x+i.y)*K2;
-                    vec2 o = (a.x>a.y) ? vec2(1.0,0.0) : vec2(0.0,1.0); //vec2 of = 0.5 + 0.5*vec2(sign(a.x-a.y), sign(a.y-a.x));
-                    vec2 b = a - o + K2;
-                    vec2 c = a - 1.0 + 2.0*K2;
-                    vec3 h = max(0.5-vec3(dot(a,a), dot(b,b), dot(c,c) ), 0.0 );
-                    vec3 n = h*h*h*h*vec3( dot(a,hash(i+0.0)), dot(b,hash(i+o)), dot(c,hash(i+1.0)));
-                    return dot(n, vec3(70.0));	
-                }
-
-                float fbm(vec2 n) {
-                    float total = 0.0, amplitude = 0.1;
-                    for (int i = 0; i < 7; i++) {
-                        total += noise(n) * amplitude;
-                        n = m * n;
-                        amplitude *= 0.4;
+                vec4 main(vec2 FC) {
+                    float e=0,R=0,t=iTime,s;
+                    vec2 r = iResolution.xy;
+                    vec3 q=vec3(0,0,-1), p, d=vec3((FC.xy-.5*r)/r.y,.7);
+                    vec4 o=vec4(0);
+                    for(float i=0;i<100;++i) {
+                        o.rgb+=hsv(.1,e*.4,e/1e2)+.005;
+                        p=q+=d*max(e,.02)*R*.3;
+                        float py = (p.x == 0 && p.y == 0) ? 1 : p.y;
+                        p=vec3(log(R=length(p))-t,e=asin(-p.z/R)-1.,atan(p.x,py)+t/3.);
+                        s=1;
+                        for(int z=1; z<=9; ++z) {
+                        e+=cos(dot(sin(p*s),cos(p.zxy*s)))/s;
+                        s+=s;
+                        }
+                        i>50.?d/=-d:d;
                     }
-                    return total;
-                }
-
-                // -----------------------------------------------
-
-                half4 main(in vec2 fragCoord ) {
-                    vec2 p = fragCoord.xy / iResolution.xy;
-                    vec2 uv = p*vec2(iResolution.x/iResolution.y,1.0);    
-                    float time = iTime * speed;
-                    float q = fbm(uv * cloudscale * 0.5);
-                    
-                    //ridged noise shape
-                    float r = 0.0;
-                    uv *= cloudscale;
-                    uv -= q - time;
-                    float weight = 0.8;
-                    for (int i=0; i<8; i++){
-                        r += abs(weight*noise( uv ));
-                        uv = m*uv + time;
-                        weight *= 0.7;
-                    }
-                    
-                    //noise shape
-                    float f = 0.0;
-                    uv = p*vec2(iResolution.x/iResolution.y,1.0);
-                    uv *= cloudscale;
-                    uv -= q - time;
-                    weight = 0.7;
-                    for (int i=0; i<8; i++){
-                        f += weight*noise( uv );
-                        uv = m*uv + time;
-                        weight *= 0.6;
-                    }
-                    
-                    f *= r + f;
-                    
-                    //noise colour
-                    float c = 0.0;
-                    time = iTime * speed * 2.0;
-                    uv = p*vec2(iResolution.x/iResolution.y,1.0);
-                    uv *= cloudscale*2.0;
-                    uv -= q - time;
-                    weight = 0.4;
-                    for (int i=0; i<7; i++){
-                        c += weight*noise( uv );
-                        uv = m*uv + time;
-                        weight *= 0.6;
-                    }
-                    
-                    //noise ridge colour
-                    float c1 = 0.0;
-                    time = iTime * speed * 3.0;
-                    uv = p*vec2(iResolution.x/iResolution.y,1.0);
-                    uv *= cloudscale*3.0;
-                    uv -= q - time;
-                    weight = 0.4;
-                    for (int i=0; i<7; i++){
-                        c1 += abs(weight*noise( uv ));
-                        uv = m*uv + time;
-                        weight *= 0.6;
-                    }
-                    
-                    c += c1;
-                    
-                    vec3 skycolour = mix(skycolour2, skycolour1, p.y);
-                    vec3 cloudcolour = vec3(1.1, 1.1, 0.9) * clamp((clouddark + cloudlight*c), 0.0, 1.0);
-                
-                    f = cloudcover + cloudalpha*f*r;
-                    
-                    vec3 result = mix(skycolour, clamp(skytint * skycolour + cloudcolour, 0.0, 1.0), clamp(f + c, 0.0, 1.0));
-                    
-                    return vec4( result, 1.0 );
+                    return o;
                 }
                 """,
 
