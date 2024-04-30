7860714a5a814b107d9d288ee1743ef95bf7ce8e
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-04-30 13:12:49 -0500

    Set `$ANDROID_NDK_HOME` in `devcontainer.json`

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,3 @@
 echo "ANDROID_HOME=$ANDROID_HOME"
 
 yes | sudo $ANDROID_HOME/cmdline-tools/latest/bin/sdkmanager "ndk;23.2.8568313" "cmake;3.22.1"
-
-export "ANDROID_NDK_HOME=$ANDROID_HOME/ndk/23.2.8568313"
-echo "ANDROID_NDK_HOME=$ANDROID_NDK_HOME"
